7cad5b93212444394cc04a03aca7b1ea83edc8d6
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2024-05-28 08:24:28 +0700

    init code

---
+++ src/main/java/com/example/frealsb/FrealSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.frealsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrealSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrealSbApplication.class, args);
+    }
+
+}
